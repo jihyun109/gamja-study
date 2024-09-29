f1159bfe890d341175742023c592f51813d629fc
jihyun109 <wlguswlgus989@gmail.com>
2024-09-29 18:45:11 +0900

    feat: User 페이징 DTO 생성

---
+++ src/main/java/gamja/gamja_pre/dto/user/response/UserPagedListResponseDTO.java
@@ -0,0 +1,18 @@
+package gamja.gamja_pre.dto.user.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserPagedListResponseDTO {
+    private Long id;
+    private String userName;
+    private String email;
+}
