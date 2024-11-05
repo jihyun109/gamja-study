636e3fb4923b5c3873eea695f07e934e1bc7d229
jihyun109 <wlguswlgus989@gmail.com>
2024-11-05 11:07:37 +0900

    refactor: 패키지명 변경

--- src/test/java/gamja/gamja_pre1/GamjaPre1ApplicationTests.java
+++ src/test/java/gamja/gamja_pre1/GamjaPre1ApplicationTests.java
@@ -1,9 +1,10 @@
-package gamja1.gamja_pre1;
+package gamja.gamja_pre1;
 
+import gamja.gamja_pre.GamjaPreApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GamjaPreApplication.class)
 class GamjaPre1ApplicationTests {
 
 	@Test
