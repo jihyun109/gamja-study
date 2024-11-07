597f3803dbee29130620243dc964d907f9f591ec
jihyun109 <wlguswlgus989@gmail.com>
2024-11-07 17:22:01 +0900

    refactor: 불필요한 빈 삭제

--- src/main/java/gamja/gamja_pre/config/Config.java
+++ src/main/java/gamja/gamja_pre/config/Config.java
@@ -1,8 +1,5 @@
 package gamja.gamja_pre.config;
 
-import gamja.gamja_pre.SecurityService;
-import gamja.gamja_pre.repository.PostRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
@@ -10,10 +7,5 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true)    // 전역 메서드 보안 활성화
 public class Config {
-
-    @Bean
-    public SecurityService securityService(PostRepository postRepository) {
-        return new SecurityService(postRepository);
-    }
 
 }
